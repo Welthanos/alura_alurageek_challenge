8a46dcf130980e15b6c2a9c960801261262467a3
Welthanos <welthanos_dev@outlook.com>
2023-07-31 18:35:32 -0300

    feat: main page structure and view folders

---
+++ script.js
@@ -0,0 +1,6 @@
+// fetch('produto.html') // Carrega o conteúdo da página "produtos.html"
+//             .then(response => response.text())
+//             .then(data => {
+//                 const produtosComponent = document.getElementById('produtos-component');
+//                 produtosComponent.innerHTML = data; // Insere o conteúdo no elemento <div>
+//             });
